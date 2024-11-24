a797aa32d682247a198da990e1eea3877c55de52
Aleksander323 <aleksander.zurowski2018@gmail.com>
2024-11-24 14:31:00 +0100

    Add files via upload

---
+++ generatory.py
@@ -0,0 +1,53 @@
+from random import randint
+from collections import deque
+
+
+def lcg(x, ln, m, a, c):
+    nums = [(x := (a*x + c) % m) / m for _ in range(ln)]
+    return nums
+
+
+def glcg(x_vec, ln, m, a_vec):
+    assert isinstance(x_vec, list)
+    assert isinstance(a_vec, list)
+    assert len(x_vec) == len(a_vec)
+    x_vec = deque(x_vec)
+    nums = []
+    for _ in range(ln):
+        x = sum(a * x for a, x in zip(a_vec, reversed(x_vec))) % m
+        x_vec.popleft()
+        x_vec.append(x)
+        nums.append(x/m)
+    return nums
+
+
+def rc4_32(K, L, r):
+    assert 32 >= L > 0 and type(L) == int
+    assert len(K) == L
+    assert r > 0 and type(r) == int
+    # KSA
+    S = list(range(32))
+    j = 0
+    for i in range(32):
+        j = (j + S[i] + K[i % L]) % 32
+        S[i], S[j] = S[j], S[i]
+    # PRGA
+    i, j = 0, 0
+    Y = []
+    for _ in range(r):
+        i = (i + 1) % 32
+        j = (j + S[i]) % 32
+        S[i], S[j] = S[j], S[i]
+        Y.append(S[(S[i] + S[j]) % 32])
+    return Y
+
+
+if __name__ == "__main__":
+    print("lcg, dane: 13, 1, 5: ", lcg(13, 10, 13, 1, 5))
+    print("lcg, dane: 2**10, 3, 7: ", lcg(13, 10, 2**10, 3, 7))
+    print('glcg: ', glcg([12, 14, 73], 10, 2**10, [3, 7, 68]))
+    print("excell's seed=0: ", lcg(0, 10, 1, 0.9821,  0.211327))
+    print("excell's seed=1812433253: ", lcg(1812433253, 10, 1, 0.9821, 0.211327))
+    L = 5
+    K = [randint(0, 31) for _ in range(L)]
+    print(rc4_32(K, L, 25))
---
+++ testy.py
@@ -0,0 +1,53 @@
+import numpy
+from scipy.stats import chisquare
+from random import randint, seed
+from generatory import rc4_32, lcg, glcg
+
+
+def test_chi2(nums, k):
+    assert isinstance(nums, (list, numpy.ndarray))
+    freq = [0] * k
+    for x in nums:
+        freq[int(x * k)] += 1
+
+    return chisquare(freq)
+
+
+def test_chi2_rc4(nums, k, n):
+    assert k <= n
+    freq = [0] * k
+    p = int(n/k)
+    for x in nums:
+        if x <= n - p:
+            freq[int(x/p)] += 1
+        else:
+            freq[-1] += 1
+
+    f_exp = [p*len(nums)/n] * (k - 1)
+    f_exp.append(len(nums) - sum(f_exp))
+
+    return chisquare(freq, f_exp)
+
+
+def scd_level(test, R, nums, *args, **kwargs):
+    n = len(nums)
+    pv_freq = [0] * 10
+    j = 0
+    for _ in range(R):
+        pv = test(nums[j:j+int(n/R)], *args, **kwargs)[1]
+        pv_freq[int(pv * 10)] += 1
+        j += int(n/R)
+
+    return chisquare(pv_freq)
+
+
+if __name__ == "__main__":
+    # seed(10)
+    a = 3
+    b = 200
+    L = 13
+    K = [randint(0, 31) for _ in range(L)]
+    # print(scd_level(test_chi2, 1024, lcg(1652, 2 ** 24, 2 ** 40, 16801, 126581), 100))
+    print("Final p_value for rc4_32:", scd_level(test_chi2_rc4, 1000, rc4_32(K, L, 10**7), 5, 32)[1])
+    # for _ in range(10):
+        # print(test_chi2(np.random.uniform, 10, size=1000))
