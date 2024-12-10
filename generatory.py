1bd5565527f1d298c36854509f9fc63e8eaefcea
Aleksander323 <aleksander.zurowski2018@gmail.com>
2024-12-10 18:56:29 +0100

    Update generatory.py

--- generatory.py
+++ generatory.py
@@ -11,34 +11,41 @@ def glcg(x_vec, ln, m, a_vec):
     assert isinstance(x_vec, list)
     assert isinstance(a_vec, list)
     assert len(x_vec) == len(a_vec)
+    
     x_vec = deque(x_vec)
-    nums = []
+    nums = [0]*ln
+    
     for _ in range(ln):
         x = sum(a * x for a, x in zip(a_vec, reversed(x_vec))) % m
         x_vec.popleft()
         x_vec.append(x)
-        nums.append(x/m)
+        nums[t] = x / m
+        
     return nums
 
 
-def rc4_32(K, L, r):
-    assert 32 >= L > 0 and type(L) == int
+def rc4_32(K, L, r, m):
+    assert m >= L > 0 and type(L) == int
     assert len(K) == L
     assert r > 0 and type(r) == int
+    
     # KSA
-    S = list(range(32))
+    S = list(range(m))
     j = 0
-    for i in range(32):
-        j = (j + S[i] + K[i % L]) % 32
+    for i in range(m):
+        j = (j + S[i] + K[i % L]) % m
         S[i], S[j] = S[j], S[i]
+        
     # PRGA
     i, j = 0, 0
-    Y = []
+    Y = [0]*r
+    
     for _ in range(r):
-        i = (i + 1) % 32
-        j = (j + S[i]) % 32
+        i = (i + 1) % m
+        j = (j + S[i]) % m
         S[i], S[j] = S[j], S[i]
-        Y.append(S[(S[i] + S[j]) % 32])
+        Y[r] = S[(S[i] + S[j]) % m]
+        
     return Y
 
 
@@ -46,8 +53,10 @@ def rc4_32(K, L, r):
 def bbs(p, q, x, ln):
     # assert is_prime(p) and is_prime(q)
     assert x % p != 0 and x % q != 0 and p % 4 == 3 and q % 4 == 3
+    
     m = p*q
-    nums = [(x := x**2 % m) / m for _ in range(ln)]
+    nums = [(x := x*x % m) / m for _ in range(ln)]
+    
     return nums
 
 
